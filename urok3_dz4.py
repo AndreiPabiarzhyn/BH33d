9f2878926526477c144d488ad9197cae03210ac8
AndreiPabiyyarzhin <a.pobyarzhin@gmail.com>
2022-08-19 11:35:56 +0300

    added a new method for task 4

--- urok3_dz4.py
+++ urok3_dz4.py
@@ -3,12 +3,19 @@
 
 negative: int = 0
 positiv: int = 0
+list: list = []
 
-for i in range(3):
-    vvod: int = int(input('введи число: '))
-    if vvod>0 :
-        positiv += 1
-    else:
-        negative +=1
-print(positiv, ' положительных чисел')
-print(negative, ' отрицательных чисел')
+# for i in range(3):
+#     vvod: int = int(input('введи число: '))
+#     if vvod>0 :
+#         positiv += 1
+#     else:
+#         negative +=1
+# print(positiv, ' положительных чисел')
+# print(negative, ' отрицательных чисел')
+
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+c = int(input("Введите третье число: "))
+print((a > 0) + (b > 0) + (c > 0), 'положительные числа')
+print((a < 0) + (b < 0) + (c < 0), 'отрицательные числа')
