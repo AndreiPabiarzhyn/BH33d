0e11497fbb00f8e4582b684d31ce08e169167b30
AndreiPabiyyarzhin <a.pobyarzhin@gmail.com>
2022-08-19 11:02:32 +0300

    add L3 cod

--- Urok3_practice.py
+++ Urok3_practice.py
@@ -1,1 +1,26 @@
-# Домашняя работа
+# не использую split, распечатай первое и последнее слово из предложения.
+# test = input(' введите предложение минимум из трех слов: ').strip()
+# first_space = test.find(' ')
+# last_space = test.rfind(' ')
+# first_word = test[:first_space]
+# last_word = test[last_space +1]
+# print(first_word)
+# print(last_word)
+
+# даны возраст отца и сына (берутся с клавиатуры)
+# через сколько лет отец будет в два раза старше сына
+
+# son = int(input('введи возраст сына'))
+# father = int(input('введи возраст отца'))
+# otvet = father - 2*(son)
+# print('через ', otvet, 'отец будет старше сына в 2 раза')
+
+# Вводится трехзначное число, найти сумму его цифр
+
+a: str = input(' Введи трех значне число: ')
+aa:int = int(a[0])
+ab:int = int(a[1])
+ac:int = int(a[2])
+result = aa + ab + ac
+print(result)
+
