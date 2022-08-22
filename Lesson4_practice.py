31b3e6e48874c213127e935b212a436f7e5e4bf6
AndreiPabiyyarzhin <a.pobyarzhin@gmail.com>
2022-08-22 23:18:16 +0300

    add some task

--- Lesson4_practice.py
+++ Lesson4_practice.py
@@ -1,11 +1,18 @@
 # Пользователь вводит число N с клавиатуры
 # посчитать сумму чисел от 1 до N
 
-n:int = int(input('введи число '))
+# n:int = int(input('введи число '))
+#
+# mnoghestvo:list =[i for i in range(1, n+1)]
+# print(mnoghestvo)
+# print(sum(mnoghestvo))
 
-mnoghestvo:list =[i for i in range(1, n+1)]
-print(mnoghestvo)
-print(sum(mnoghestvo))
+# some_list = [1, 2, 31]
+# del some_list[0]
+# print(some_list)
+# some_list = [1, 2, 31]
+# some_list.remove(1)
+# print(some_list)
 
 
 
