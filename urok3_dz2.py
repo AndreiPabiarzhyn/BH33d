6db3b64ff7192f9c180225c20a452dbf6cfc4c73
AndreiPabiyyarzhin <a.pobyarzhin@gmail.com>
2022-08-22 23:20:59 +0300

    add task1

--- urok3_dz2.py
+++
@@ -1,8 +0,0 @@
-# Пользователь вводит 3 числа, найти среднее арифметическое с точностью 3
-summa:int= 0
-
-for i inrange(3):
-    vvod_chisla:int=int(input("введи число: "))
-    summa = summa + vvod_chisla
-otvet:float= summa/3
-print(round(otvet, 3))
