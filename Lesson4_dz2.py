fffd81bca5f685a12e799c5a10e4ed57717eb67e
AndreiPabiyyarzhin <a.pobyarzhin@gmail.com>
2022-08-23 00:32:33 +0300

    add task2

--- Lesson4_dz2.py
+++ Lesson4_dz2.py
@@ -1,6 +1,16 @@
 # Без использования collections, написать программу, которая будет
 # создавать словарь для подсчитывания количества вхождений каждой
 # буквы в текст введенный с клавиатуры
-text: str = input('введи слово: ')
-letters: list = text.split()
-print(letters)
+
+# import collections
+
+# results = collections.Counter(text)
+# print(results)
+
+text: str = input('введи текст: ')
+# letters: list = text.split()
+print(text)
+# rezult = dict((i, text.count(i)) for i in set(text))
+rezult: dict = dict((i, text.count(i)) for i in set(text))
+print(rezult)
+
