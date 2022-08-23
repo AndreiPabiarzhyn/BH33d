6116310962c71a7961346250c50b0edfc1cf93d1
AndreiPabiyyarzhin <a.pobyarzhin@gmail.com>
2022-08-23 11:50:27 +0300

    add task

--- Lesson5_practice.py
+++ Lesson5_practice.py
@@ -1,12 +1,26 @@
-x = True
-y = False
-z = False
+# x = True
+# y = False
+# z = False
+#
+# if not x or y:
+#     print(1)
+# elif not x or not y and z:
+#     print(2)
+# elif not x or y or not y and x:
+#     print(3)
+# else:
+#     print(4)
 
-if not x or y:
-    print(1)
-elif not x or not y and z:
-    print(2)
-elif not x or y or not y and x:
-    print(3)
-elif:
-    print(4)
+# вводится слово. необхадимо определить, является ли оно полиндромом
+
+# slovo = input().lower().replace(' ', '')
+# razvorot = slovo[::-1]
+# if razvorot in slovo:
+#     print('yes')
+# else:
+#     print('no')
+
+
+
+
+
