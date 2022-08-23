07db65005b16abcc990a1bf305e5e28dc53eac51
AndreiPabiyyarzhin <a.pobyarzhin@gmail.com>
2022-08-23 19:34:01 +0300

    add task

--- Lesson5_dz1.py
+++ Lesson5_dz1.py
@@ -1,12 +1,11 @@
 # Дан список чисел, отсортировать его по возрастанию
 # без использования sort и sorted
+spisok = [1, 5, 76, 23, 645, 33, 78]
 
-n = 1
+for _ in spisok:
+    for i in range(len(spisok)-1):
+        if spisok[i] > spisok[i+1]:
+            spisok[i], spisok[i+1] = spisok[i+1], spisok[i]
+print(spisok)
 
-# while n < len(spisok):
-#     for i in range(len(spisok)-n):
-#         if spisok[i] > spisok[i+1]:
-#             spisok[i], spisok[i+1] = spisok[i+1], spisok[i]
-#     n += 1
-print(n)
 
