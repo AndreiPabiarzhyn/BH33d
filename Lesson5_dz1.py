37cccc014eb69ef4a6960eaa1c9ff4afc7f1155e
AndreiPabiyyarzhin <a.pobyarzhin@gmail.com>
2022-08-26 10:30:16 +0300

    add task

--- Lesson5_dz1.py
+++ Lesson5_dz1.py
@@ -3,8 +3,9 @@
 spisok = [1, 5, 76, 23, 645, 33, 78]
 print(spisok[2])
 
-# for _ in spisok:
-#     for i in range(len(spisok)-1):
-#         if spisok[i] > spisok[i+1]:
-#             spisok[i], spisok[i+1] = spisok[i+1], spisok[i]
-# print(spisok)
+for _ in spisok:
+    for i in range(len(spisok)-1):
+        if spisok[i] > spisok[i+1]:
+            spisok[i], spisok[i+1] = spisok[i+1], spisok[i]
+print(spisok)
+
