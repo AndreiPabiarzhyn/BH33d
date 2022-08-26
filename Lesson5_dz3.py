b7280b1a7ca6edd490f3e1992c367d4c8ee5b48c
AndreiPabiyyarzhin <a.pobyarzhin@gmail.com>
2022-08-26 11:34:02 +0300

    add task

---
+++ Lesson5_dz3.py
@@ -0,0 +1,26 @@
+# Сделать калькулятор: у пользователя
+# спрашивается число, потом действие и второе число
+# while True:
+
+number_1 = int(input('введи первое число: '))
+action = input('введи действие (деление, умножение, сложение, разность): ')
+number_2 = int(input('введи второе число: '))
+while True:
+    if action == 'деление' or action == '/':
+        print(f'{number_1}/{number_2} = {number_1 / number_2}')
+    elif action == 'умножение' or action == '*':
+        print(f'{number_1}*{number_2} = {number_1 * number_2}')
+    elif action == 'сложение' or action == '+':
+        print(f'{number_1}+{number_2} = {number_1 + number_2}')
+    elif action == 'разность' or action == '-':
+        print(f'{number_1}-{number_2} = {number_1 - number_2}')
+    else:
+        print('не ттакого действия')
+
+
+
+
+
+
+
+
