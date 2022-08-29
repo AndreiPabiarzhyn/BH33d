e9680b8710fa783d2572772d423d527a19f07119
AndreiPabiyyarzhin <a.pobyarzhin@gmail.com>
2022-08-30 01:57:46 +0300

    add print(list) in Lesson1_dz1

--- Lesson5_dz1.py
+++ Lesson5_dz1.py
@@ -1,9 +1,10 @@
 # Вывести первые N цисел кратные M и больше K
 lower = int(input('ввести нижний диапозон: '))
-upper = int(input('ввести нижний диапозон: '))
+upper = int(input('ввести верхний диапозон: '))
 m = int(input('ввести делитель: '))
 k = int(input('введи число: '))
+print([i for i in range(lower, upper+1)])
 
-for i in range(lower,upper+1):
+for i in range(lower, upper+1):
     if i%m == 0 and i>k:
         print(i)
