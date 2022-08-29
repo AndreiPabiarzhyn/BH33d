c6d18caccbb54e84df1f4a963bf5fcb4224337e9
AndreiPabiyyarzhin <a.pobyarzhin@gmail.com>
2022-08-29 22:04:09 +0300

    add task

--- Lesso6_practice.py
+++ Lesso6_practice.py
@@ -1,24 +1,43 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# # Пример как работает LIFO
+# numbers = []
+#
+# for i in range(5):
+#     numbers.append(i)
+#     print(numbers)
+#
+# while numbers:
+#     del numbers[-1]
+#     print(numbers)
+#
+# # Пример как работает FIFO
+# numbers = []
+#
+# for i in range(5):
+#     numbers.append(i)
+#     print(numbers)
+#
+# while numbers:
+#     del numbers[0]
+#     print(numbers)
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
