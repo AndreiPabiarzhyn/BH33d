1c1e96d64b61f6420f8dd399f5b9b731e10c6ad3
AndreiPabiyyarzhin <a.pobyarzhin@gmail.com>
2022-08-30 01:51:07 +0300

    add task

--- Lesson6_practice.py
+++ Lesson6_practice.py
@@ -8,7 +8,7 @@
 # while numbers:
 #     del numbers[-1]
 #     print(numbers)
-#
+
 # # Пример как работает FIFO
 # numbers = []
 #
@@ -19,25 +19,29 @@
 # while numbers:
 #     del numbers[0]
 #     print(numbers)
+
+# def is_palindrome(text:str) -> None:
+#     text = text.lower().replace(' ', '')
+#     return text == text[::-1]
 #
+# word = input()
+# result = is_palindrome(word)
+# print(result)
+
+# пример возвращения не только булевской переменной, но и саму переменную
+# def is_palindrome(text:str):
+#     text = text.lower().replace(' ', '')
+#     return text == text[::-1], text
 #
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
+# word = input()
+# result = is_palindrome(word)
+# print(result)
+
+# аргументы со значением по умолчанию
+def foo(a, b, *args):
+        print(a, b, args)
+
+
+foo(4, 45, 6, 7, 8, 9, 9, 6,)
+
+
