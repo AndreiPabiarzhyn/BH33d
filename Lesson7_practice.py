42feef3da21e83cd6e8d7d649617b9d54739366e
AndreiPabiyyarzhin <a.pobyarzhin@gmail.com>
2022-09-01 16:42:53 +0300

    add project

--- Lesson7_practice.py
+++ Lesson7_practice.py
@@ -1,10 +1,49 @@
-class User:
-        def __init__(self, name: str, email: str) -> None:
-            self. first_name = name.title()
-            self. email = email.lower()
+# class User:
+#         def __init__(self, name: str, email: str) -> None:
+#             self. first_name = name.title()
+#             self. email = email.lower()
+#
+# vasya = User(
+#     name='vasya',
+#     email='vasya@info.com'
+# )
+# print(vasya.email)
 
-vasya = User(
-    name='vasya',
-    email='vasya@info.com'
-)
-print(vasya.email)
+import random
+
+# films: list = ["Душа", "Золушка", "Мулан", "Эверест", "Гринч", "Моана", "Зверополис", "Головоломка"]
+# print(films[2])
+#
+# while True:
+#     command = input('введи команду: ')
+#     if command == 'заменить':
+#         replace_film = int(input('введи какой фильм заменить: '))
+#         new_film = input('введи новый фильм: ')
+#         films[replace_film] = new_film
+#     print(films)
+
+
+# films = ["Душа", "Золушка", "Мулан", "Эверест", "Гринч", "Моана", "Зверополис", "Головоломка"]
+#
+# while True:
+#     command = input('введи команду: ')
+#     if command == 'заменить':
+#         replace_film = input('введи какой фильм заменить: ')
+#         if replace_film in films:
+#             new_film = input('введи новый фильм: ')
+#             for n, i in enumerate(films):
+#                 if i == replace_film:
+#                     films[n] = new_film
+#         else:
+#             print(" нет такого фильма в списке")
+#
+#     print(films)
+
+slovar_name = {
+    2: 'Vasy',
+    1: 'Fdg',
+
+}
+
+
+
