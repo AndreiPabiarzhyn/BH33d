181b02d0f4932e5507e758cfd72b31cbc5f6cc2a
AndreiPabiyyarzhin <a.pobyarzhin@gmail.com>
2022-10-09 14:57:39 +0300

    add project

--- Lesson7_practice.py
+++ Lesson7_practice.py
@@ -1,49 +1,61 @@
-# class User:
-#         def __init__(self, name: str, email: str) -> None:
-#             self. first_name = name.title()
-#             self. email = email.lower()
-#
-# vasya = User(
-#     name='vasya',
-#     email='vasya@info.com'
-# )
-# print(vasya.email)
+import pgzrun
+# pgzero
 
-import random
+WIDTH = 600  # Ширина окна
+HEIGHT = 300  # Высота окна
 
-# films: list = ["Душа", "Золушка", "Мулан", "Эверест", "Гринч", "Моана", "Зверополис", "Головоломка"]
-# print(films[2])
-#
-# while True:
-#     command = input('введи команду: ')
-#     if command == 'заменить':
-#         replace_film = int(input('введи какой фильм заменить: '))
-#         new_film = input('введи новый фильм: ')
-#         films[replace_film] = new_film
-#     print(films)
+TITLE = "Инопланетный раннер"  # Заголовок окна игры
+FPS = 30  # Количество кадров в секунду
+
+# Объекты
+alien = Actor('alien', (50, 240))
+fon = Actor("fon")
+box = Actor('box', (550, 265))
+bee = Actor('bee', (850, 175))  # Пчела
 
 
-# films = ["Душа", "Золушка", "Мулан", "Эверест", "Гринч", "Моана", "Зверополис", "Головоломка"]
-#
-# while True:
-#     command = input('введи команду: ')
-#     if command == 'заменить':
-#         replace_film = input('введи какой фильм заменить: ')
-#         if replace_film in films:
-#             new_film = input('введи новый фильм: ')
-#             for n, i in enumerate(films):
-#                 if i == replace_film:
-#                     films[n] = new_film
-#         else:
-#             print(" нет такого фильма в списке")
-#
-#     print(films)
-
-slovar_name = {
-    2: 'Vasy',
-    1: 'Fdg',
-
-}
+def draw():
+    fon.draw()
+    alien.draw()
+    box.draw()
+    bee.draw()
 
 
+def update(dt):
+    # Движение пчелы
+    if bee.x > -20:
+        bee.x = bee.x - 5
+    else:
+        bee.x = WIDTH + 20
+
+    # Движение коробки
+    if box.x > -20:
+        box.x = box.x - 5
+        box.angle = box.angle + 5
+    else:
+        box.x = WIDTH + 20
+
+    # Управление
+    if keyboard.left or keyboard.a and alien.x > 20:
+        alien.x = alien.x - 5
+        alien.image = 'left'
+    elif keyboard.right or keyboard.d and alien.x < 580:
+        alien.x = alien.x + 5
+        alien.image = 'right'
+    elif keyboard.down or keyboard.s:
+        alien.image = 'duck'
+        alien.y = 250
+    else:
+        alien.image = 'alien'
+        if alien.y > 240:
+            alien.y = 240
+
+
+def on_key_down(key):
+    # Прыжок
+    if keyboard.space or keyboard.up or keyboard.w:
+        alien.y = 100
+        animate(alien, tween='bounce_end', duration=2, y=240)
+
+pgzrun.go()
 
